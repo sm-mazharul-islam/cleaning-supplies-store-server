05a9fa6bfefd287bd80aaa3b7002a81b9bf42213
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2026-04-24 00:11:31 +0600

    update: vercel json

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: true, // এটি সব অরিজিনকে অটোমেটিক এলাউ করবে
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  }),
+);
 app.use(express.json());
 
 // MongoDB Connection URL
