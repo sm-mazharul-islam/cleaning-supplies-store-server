136d02c981aaeaeb3fb31c8d177486687deaf47b
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2026-04-24 00:06:00 +0600

    Update: cors

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://cleaning-supplies-store-0.vercel.app",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // MongoDB Connection URL
