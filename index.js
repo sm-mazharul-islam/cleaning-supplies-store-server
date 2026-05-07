699becc2a8e3c31d6cba7a82eac6fb26f15ada1e
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2026-05-07 20:10:28 +0600

    Update: index

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const port = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     console.log("⏳ Connecting to Database...");
-    // ১. আগে ডাটাবেজ কানেক্ট করুন
     await connectToDatabase();
 
-    // ২. কানেকশন সফল হলে তবেই সার্ভার চালু করুন
     app.listen(port, () => {
       console.log(
         `🚀 Database Connected & Server running on http://localhost:${port}`,
