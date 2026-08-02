afcca7ee987f8d62c8fecb7d5c8ddda28ff60d2f
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2026-08-03 00:02:22 +0600

    refactor(testimonial): update testimonial controller logic

--- src/controllers/testimonial.controller.js
+++ src/controllers/testimonial.controller.js
@@ -1,57 +1,34 @@
-const connectToDatabase = require("../config/db");
+const Testimonial = require("../models/testimonial.model");
 
-// ---  (POST) ---
 exports.createTestimonial = async (req, res) => {
   try {
-    const { db } = await connectToDatabase();
-    const TestimonialCollection = db.collection("testimonials");
-
     const { comment, rating } = req.body;
-
     if (!req.user) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized: User context missing",
-      });
+      return res.status(401).json({ success: false, message: "Unauthorized: User context missing" });
     }
 
-    const newTestimonial = {
+    const newTestimonial = await Testimonial.create({
       comment,
       rating: Number(rating),
       userId: req.user.id || req.user._id,
       userName: req.user.userName,
       userImage: req.user.pictureUrl || "",
-      createdAt: new Date(),
-    };
-
-    const result = await TestimonialCollection.insertOne(newTestimonial);
+    });
 
     res.status(201).json({
       success: true,
       message: "Review added successfully!",
-      data: { ...newTestimonial, _id: result.insertedId },
+      data: newTestimonial,
     });
   } catch (error) {
-    console.error("🔥 Testimonial Error:", error.message);
     res.status(500).json({ success: false, message: error.message });
   }
 };
 
-//  (GET) ---
 exports.getTestimonials = async (req, res) => {
   try {
-    const { db } = await connectToDatabase();
-    //(Sorting)
-    const result = await db
-      .collection("testimonials")
-      .find({})
-      .sort({ createdAt: -1 })
-      .toArray();
-
-    res.status(200).json({
-      success: true,
-      data: result,
-    });
+    const result = await Testimonial.find({}).sort({ createdAt: -1 });
+    res.status(200).json({ success: true, data: result });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
