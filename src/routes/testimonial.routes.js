897513a76a6ffb263c31988fd6253c604cbc9bdd
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2026-08-03 00:03:17 +0600

    refactor(routes): update testimonial route definitions

--- src/routes/testimonial.routes.js
+++ src/routes/testimonial.routes.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const testimonialController = require("../controllers/testimonial.controller");
-
 const { verifyToken } = require("../middlewares/auth.middleware");
 
 router.get("/", testimonialController.getTestimonials);
-
 router.post("/", verifyToken, testimonialController.createTestimonial);
 
 module.exports = router;
