c3197b871a86c8488edf5a00121219eaf2cb69e3
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2026-08-03 00:03:10 +0600

    refactor(routes): update comment route definitions

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {
-  addComment,
-  getCommentsByProduct,
-  addReply,
-} = require("../controllers/comment.controller");
+const { addComment, getCommentsByProduct, addReply } = require("../controllers/comment.controller");
 
 router.post("/", addComment);
 router.get("/:productId", getCommentsByProduct);
