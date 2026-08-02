747d23241474ae32cf14a188308ecf39d9bf096f
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2026-08-03 00:03:06 +0600

    refactor(models): update category schema model

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema(
   {
-    id: { type: String, required: true, unique: true }, // Slug format (e.g., 'industrial-care')
+    id: { type: String, unique: true },
     name: { type: String, required: true },
     description: { type: String },
-    iconName: { type: String, required: true }, // icons/fa থেকে নাম আসবে (e.g., 'FaTools')
+    iconName: { type: String },
     gradient: { type: String, default: "from-blue-600/10 to-transparent" },
     count: { type: Number, default: 0 },
   },
-  { timestamps: true },
+  { timestamps: true, collection: "categories" }
 );
 
 module.exports = mongoose.model("Category", categorySchema);
